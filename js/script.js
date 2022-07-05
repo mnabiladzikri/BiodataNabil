bf95381389e00e846bfc3e2ade50d7156cd78761
Muhammad Nabil <mnabiladzdzikri@gmail.com>
2022-07-05 18:19:15 +0700

    major update html 05/07/2022 18:18

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
+
+$('.page-scroll').on('click', function(e) {
+
+    var tujuan = $(this).attr('href');
+   
+    var elemenTujuan = $(tujuan);
+   
+    $('html , body').animate({
+     scrollTop: elemenTujuan.offset().top -100
+    });
+   
+    e.preventDefault();
+   });
+
